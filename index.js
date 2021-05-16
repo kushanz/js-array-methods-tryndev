52732cb155c237577bbd4e2fec2b9cbaffad2e7f
kushanz <kushanshanuka@yahoo.com>
2021-05-16 08:07:45 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h3>JS Array methods</h3>`;
+
+// array sample
+var products = [
+  { name: 'Iron', price: 530 },
+  { name: 'Phone', price: 770 },
+  { name: 'Laptop', price: 2300 },
+  { name: 'Toy', price: 250 },
+  { name: 'Pen', price: 40 },
+  { name: 'Book', price: 120 },
+  { name: 'Table', price: 900 },
+  { name: 'Shoes', price: 650 }
+];
